be352ad9722af73e9423377e2699e42de9e46a62
Mike Koss <koss@google.com>
2015-04-20 15:04:35 -0700

    wire buttons

--- demo/firebase-demo.js
+++ demo/firebase-demo.js
@@ -1,5 +1,24 @@
 window.addEventListener('load', initDemo);
 
 function initDemo() {
-  console.log("Demo init.");
+  bindButton('change-nickname', onChangeNickname);
+  bindButton('send-message', onSendMessage);
+}
+
+function onChangeNickname() {
+  var name = getValue('nickname');
+  console.log("Change nickname to " + name);
+}
+
+function onSendMessage() {
+  var message = getValue('message');
+  console.log("Send message: " + message);
+}
+
+function bindButton(id, fn) {
+  document.getElementById(id).addEventListener('click', fn);
+}
+
+function getValue(id) {
+  return document.getElementById(id).value;
 }
